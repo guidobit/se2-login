cb9b26af9f1dbfef731777f53741fc2f3f3ffc38
guidobit <guido@bit-students.com>
2017-11-27 20:23:35 +0100

    Added port to development.sh because it doesnt work otherwise

--- development.sh
+++ development.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # This runs the container in development mode
-docker run -it -v $(pwd -LP):/app se2-login:latest /bin/sh
+docker run -it -v $(pwd -LP):/app -p 80:5000 se2-login:latest /bin/sh
