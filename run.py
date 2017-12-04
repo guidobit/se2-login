0482b33a93b5e6306479ad44329bb622ff3cd7a8
guidobit <guido@bit-students.com>
2017-12-04 19:01:35 +0100

    Added endpoints

--- run.py
+++ run.py
@@ -1,8 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, request, Response
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World! Lets go!"
 
-print('Hello world')
+@app.route("/", methods=["GET"])
+def hello():
+    return "Hello World! Lets go, test!"
+
+
+@app.route("/api/v1/login", methods=["POST"])
+def login():
+    return "login api"
+
+
+@app.route("/api/v1/register", methods=["POST"])
+def login():
+    return "register api"
+
+
+@app.route("/api/v1/logout", methods=["GET"])
+def login():
+    return "logout api"
+
+
+@app.route("/api/v1/validate/token", methods=["POST"])
+def login():
+    return "validate/token api"
+
+
+@app.route("/api/v1/publickey", methods=["GET"])
+def login():
+    return "publickey api"
+
+
+if __name__ == "__main__":
+    wildcard_interface = "0.0.0.0" # Runs on all available interfaces
+    app.run(debug=True, host=wildcard_interface)
+
+
+print('App started')
